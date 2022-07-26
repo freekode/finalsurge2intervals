e40fd67a27da7b6b799e8bf4da77390940af10ae
Evgeny <iam@freekode.org>
2022-07-26 12:40:40 +0200

    past days configurable

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ CONFIG_FILENAME = 'config.yaml'
 
 def main():
     config = get_config()
-    finalsurge_api = api.FinalSurgeApi(config['finalsurge']['username'], config['finalsurge']['password'], config['finalsurge']['sync_past_days'])
+    finalsurge_api = api.FinalSurgeApi(config['finalsurge']['username'], config['finalsurge']['password'])
     finalsurge_api.login()
 
     intervals_api = api.IntervalsApi(config['intervals']['api_key'], config['intervals']['athlete_id'])
 
-    synchronizer = sync.Synchronizer(finalsurge_api, intervals_api)
+    synchronizer = sync.Synchronizer(finalsurge_api, intervals_api, config['sync_past_days'])
     synchronizer.sync_values([sync.SyncValueType.HRV, sync.SyncValueType.RHR, sync.SyncValueType.WEIGHT])
 
 def get_config():
